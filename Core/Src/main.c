067c34910d5381f3129103f484314d354003c359
Suphakon Jarujunawong <xsoxkill123@gmail.com>
2021-03-14 18:53:47 +0700

    Bug fixes 3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -48,9 +48,10 @@ UART_HandleTypeDef huart2;
 /* USER CODE BEGIN PV */
 uint32_t ADCData[4] = { 0 };
 uint32_t ButtonTimeStamp = 0;
-float A = 0;
-float B = 0;
-uint32_t LED = 0;
+uint32_t release = 0;
+uint32_t TimeCode = 0;
+uint32_t ResponseGame = 0;
+int ModeLED = 0;
 GPIO_PinState SwitchState[2];
 
 /* USER CODE END PV */
@@ -110,13 +111,13 @@ int main(void) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-		/*if (LED == 1);
-		{
-			if(HAL_GetTick() - ButtonTimeStamp >=  (1000 + ((22695477 * ADCData[0]) + ADCData[1]) % 10000))
-			{
+		if (ModeLED == 1) {
+			if ((HAL_GetTick() - ButtonTimeStamp)
+					>= (1000 + ((22695477 * ADCData[0]) + ADCData[1]) % 10000)) {
+				ResponseGame = (1000 + ((22695477 * ADCData[0]) + ADCData[1]) % 10000);
 				HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 			}
-		}*/
+		}
 	}
 	/* USER CODE END 3 */
 }
@@ -315,24 +316,20 @@ static void MX_GPIO_Init(void) {
 /* USER CODE BEGIN 4 */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	SwitchState[0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
-	B = HAL_GetTick();
+	TimeCode = HAL_GetTick();
 	if (GPIO_Pin == GPIO_PIN_13) {
 		//if (SwitchState[1] == 1 && SwitchState[0] == 0) {
-			if (LED != 1){
-				ButtonTimeStamp = HAL_GetTick();
-				HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-			}
-			LED = 1;
-			/*if (ButtonTimeStamp
-					>= (1000 + ((22695477 * ADCData[0]) + ADCData[1]) % 10000)) {
-				HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-			}*/
+		if (ModeLED != 1) {
+			ButtonTimeStamp = HAL_GetTick();
+			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		}
+		ModeLED = 1;
 
-		 if (SwitchState[1] == 0 && SwitchState[0] == 1) {
-			A = HAL_GetTick() - ButtonTimeStamp;
-			LED = 0;
+		if (SwitchState[1] == 0 && SwitchState[0] == 1) {
+			release = HAL_GetTick() - ButtonTimeStamp;
+			ModeLED = 0;
 		}
-		}
+	}
 	SwitchState[1] = SwitchState[0];
 }
 /* USER CODE END 4 */
